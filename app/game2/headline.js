3862e3571290cdfe719d93801a51619703828775
Joe Baumgartel <josephbaumgartel@gmail.com>
2023-09-11 03:26:51 -0700

    Complete fetch on submit, check answers, and display resutls. Next step filter headlines from API to invalidate records with missing data, then begin authentication.

--- app/game2/headline.js
+++ app/game2/headline.js
@@ -1,13 +1,12 @@
 "use client";
 import { useEffect, useState } from "react";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Unstable_Grid2";
 import Image from "next/image";
+import PublicationForm from "./publicationForm";
+import HeadlineButton from "./getHeadlineButton";
 
 var photo = "/image-not-found.png";
 
@@ -24,6 +23,23 @@ export default function Headline() {
         setLoading(false);
       });
   }, []);
+
+  const fetchOnClick = () => {
+    photo = "/image-not-found.png";
+    fetch("http://localhost:3000/headlines")
+      .then((res) => res.json())
+      .then((headlines) => {
+        setheadlines(headlines);
+        console.log(headlines);
+        setLoading(false);
+      });
+    if (headlines[0].photo_source_url != null) {
+      photo = headlines[0].photo_source_url;
+      if (headlines[0].photo_source_url.slice(0, 1) != "h") {
+        photo = "https://" + headlines[0].photo_source_url;
+      }
+    }
+  };
 
   if (isLoading)
     return (
@@ -52,14 +68,14 @@ export default function Headline() {
     <>
       <Container sx={{ mt: 30 }} maxWidth="lg">
         <Grid direction="row" justifyContent="center" container spacing={2}>
-          <Grid xs={6}>
+          <Grid xs={12} md={6}>
             <Card sx={{ boxShadow: 3 }}>
               <CardHeader title=<h2 className="font-bold text-2xl">{headlines[0].headline}</h2>></CardHeader>
-              <Image alt="" src={photo} width={720} height={405} />
+              <Image priority={true} alt="" src={photo} width={720} height={405} />
             </Card>
           </Grid>
-          <Grid xs={6}>
-            <Button variant="contained">Submit</Button>
+          <Grid className="text-center" xs={12} md={6}>
+            <PublicationForm headlines={headlines} fetchOnClick={fetchOnClick}></PublicationForm>
           </Grid>
         </Grid>
       </Container>
---
+++ app/game2/publicationForm.js
@@ -0,0 +1,69 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormLabel from "@mui/material/FormLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import HeadlineButton from "./getHeadlineButton";
+
+export default function PublicationForm({ headlines, fetchOnClick }) {
+  const [value, setValue] = React.useState("");
+  const [status, setStatus] = React.useState("newForm");
+
+  const handleRadioChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (value === headlines[0].publication) {
+      setStatus("correctAnswer");
+    } else {
+      setStatus("wrongAnswer");
+    }
+  };
+
+  const getNextHeadline = () => {
+    fetchOnClick();
+    setStatus("newForm");
+    setValue("");
+  };
+
+  if (status === "correctAnswer") {
+    return (
+      <>
+        <h1 className="mt-20">You got it right. Great job!</h1>
+        <div className="mt-5">
+          <HeadlineButton content={"Next"} onClick={getNextHeadline}></HeadlineButton>
+        </div>
+      </>
+    );
+  } else if (status === "wrongAnswer") {
+    return (
+      <>
+        <h1 className="mt-20">Wrong answer. Better luck next time.</h1>
+        <div className="mt-5">
+          <HeadlineButton content={"Next"} onClick={getNextHeadline}></HeadlineButton>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormControl sx={{ mt: 10 }} variant="standard">
+        <FormLabel id="publication-radio-group-label">Guess the news source:</FormLabel>
+        <RadioGroup aria-labelledby="publication-radio-button-group" name="publication-radio-button-group" value={value} onChange={handleRadioChange}>
+          <FormControlLabel value="cnn" control={<Radio />} label="CNN" />
+          <FormControlLabel value="fox news" control={<Radio />} label="Fox News" />
+        </RadioGroup>
+        <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="contained">
+          Submit Guess
+        </Button>
+      </FormControl>
+    </form>
+  );
+}
