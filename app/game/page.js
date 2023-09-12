5c45da581d9f3fec6341355826215b9a9984e15b
Joe Baumgartel <josephbaumgartel@gmail.com>
2023-09-12 04:58:25 -0700

    Add signup and login boilerplate.

--- app/game/page.js
+++ app/game/page.js
@@ -1,6 +1,21 @@
 "use client";
 import Headline from "app/game/headline.js";
 import AppBarLoggedIn from "app/components/app-bar/appBarLoggedIn.js";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {"Copyright © "}
+      <Link color="inherit" href="/game">
+        Headline Fights
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 export default function Home() {
   return (
@@ -8,6 +23,7 @@ export default function Home() {
       <main>
         <AppBarLoggedIn></AppBarLoggedIn>
         <Headline></Headline>
+        <Copyright sx={{ mt: 5 }} />
       </main>
     </>
   );
